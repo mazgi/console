ed71360f1f1fce93fe552d1608e16d73d0d64c7d
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2020-02-16 09:30:40 +0000

    [breaking change] replace db migration script.

--- bff/src/db/migrations/1581607313540-CreateResource.ts
+++ bff/src/db/migrations/1581607313540-CreateResource.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class CreateResource1580309144281 implements MigrationInterface {
-  name = 'CreateResource1580309144281'
+export class CreateResource1581607313540 implements MigrationInterface {
+  name = 'CreateResource1581607313540'
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      'CREATE TABLE `resource` (`id` varchar(36) NOT NULL, `name` varchar(255) NOT NULL, `description` text NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
+      'CREATE TABLE `resource` (`id` varchar(36) NOT NULL, `name` varchar(255) NOT NULL, `description` text NULL, `data` json NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
       undefined
     )
   }
